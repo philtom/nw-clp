7de5b3ea0f0b816518727ddcd4773eb9d7d0f210
Phil Tom <phil@retrofitme.com>
2013-10-25 14:43:01 -0500

    created ActionTree to track actions by owner and source.  moved totaling out of DOM and into action tree

--- src-web/damage-board.js
+++ src-web/damage-board.js
@@ -6,6 +6,7 @@ nwclp.DamageBoard = function(el, file) {
   this.lastRead = 0,
   this.leftOver = "",
   this.clp = new nwclp.CombatLogParser(),
+  this.actionTree = new nwclp.ActionTree(),
   this.incrementalRead = function() {
     // reading at most 1 MB into memory at a time.  might need to optimise this buffer size.
     var endByte = Math.min(self.lastRead + 1048576, file.size),
@@ -56,19 +57,13 @@ nwclp.DamageBoard = function(el, file) {
       var owner = entries[0].owner;
       var row = self.getOrCreateRow(owner);
       var damageDoneCell = $(row).children(".damage-done")[0];
-      var damageDone = parseFloat($(damageDoneCell).attr("value"));
       var healsDoneCell = $(row).children(".heals-done")[0];
-      var healsDone = parseFloat($(healsDoneCell).attr("value"));
-      entries.forEach(function(entry) {
-        damageDone = damageDone + parseFloat(self.damage(entry));
-        healsDone = healsDone + parseFloat(self.heals(entry));
-      });
 
-      $(damageDoneCell).text(parseInt(damageDone));
-      $(damageDoneCell).attr("value", damageDone);
+      entries.forEach(self.actionTree.addEvent);
 
-      $(healsDoneCell).text(parseInt(healsDone));
-      $(healsDoneCell).attr("value", healsDone);
+      $(damageDoneCell).text(parseInt(self.actionTree.damageForOwner(id)));
+
+      $(healsDoneCell).text(parseInt(self.actionTree.healsForOwner(id)));
     }
   },
 
@@ -91,8 +86,8 @@ nwclp.DamageBoard = function(el, file) {
     $(row).addClass("row");
     $(row).attr("name", name);
     $(row).append(self.createCell(name, name, "name"));
-    $(row).append(self.createCell(parseInt(damageDone), damageDone, "damage-done"));
-    $(row).append(self.createCell(parseInt(healsDone), healsDone, "heals-done"));
+    $(row).append(self.createCell(parseInt(damageDone), "damage-done"));
+    $(row).append(self.createCell(parseInt(healsDone), "heals-done"));
     $(self.el).append(row)
 
     $(self.el).children().sort(function(a,b) {
@@ -104,12 +99,11 @@ nwclp.DamageBoard = function(el, file) {
     return row;
   },
 
-  this.createCell = function(text, value, type) {
+  this.createCell = function(text, type) {
     var cell = document.createElement("div");
     $(cell).addClass("cell");
     $(cell).addClass(type);
     $(cell).text(text);
-    $(cell).attr("value", value);
     return cell;
   },
 
@@ -127,6 +121,61 @@ nwclp.DamageBoard = function(el, file) {
     } else {
       return 0;
     }
+  };
+}
+
+nwclp.ActionTree = function() {
+  var self = this;
+  // owner
+  //  source
+  //    action = value
+  this.actionsByOwner = {},
+  this.addEvent = function(event) {
+    var bySource = self.actionsByOwner[event.owner.id];
+    if (bySource == null) {
+      bySource = {};
+      self.actionsByOwner[event.owner.id] = bySource;
+    }
+
+    var byAction = bySource[event.source.id];
+    if (byAction == null) {
+      byAction = {};
+      bySource[event.source.id] = byAction;
+    }
+
+    var value = byAction[event.event.id];
+    if (value == null) {
+      value = 0;
+    }
+
+    value = parseFloat(value) + parseFloat(event.event.value);
+    byAction[event.event.id] = value;
+  },
+
+  this.damageForOwner = function(id) {
+    var sourceActions = self.actionsByOwner[id];
+    return _.reduce(_.values(sourceActions), function(total, actions) {
+      return _.reduce(_.values(actions), function(t, value) {
+        if (value > 0) {
+          return t + value;
+        } else {
+          return t;
+        }
+      }, total);
+    }, 0);
+  },
+
+  this.healsForOwner = function(id) {
+    var sourceActions = self.actionsByOwner[id];
+    return _.reduce(_.values(sourceActions), function(total, actions) {
+      return _.reduce(_.values(actions), function(t, value) {
+        if (value < 0) {
+          return t - value;
+        } else {
+          return t;
+        }
+      }, total);
+    }, 0);
   };
 
 }
