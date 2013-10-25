4cd742ecda0821552dab938745b354bc861e090d
Phil Tom <phil@retrofitme.com>
2013-10-24 19:47:13 -0500

    refactored clp to put objects in namespace

--- src-web/nw-clp.js
+++ src-web/nw-clp.js
@@ -1,67 +1,62 @@
 var nw = new Object();
 
 nw.clp = new function() {
- this.parseCombatLogEntry = function (log) {
-   var timeEntry = log.split(/::/),
-       timestamp = this.parseTimestamp(timeEntry[0]);
-       tokens = timeEntry[1].split(/,/);
-       owner = this.createActor(tokens[0], tokens[1]),
-       source = this.createActor(tokens[2], tokens[3]),
-       target = this.createActor(tokens[4], tokens[5]),
-       event = this.createEvent(tokens[6], tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]),
-       log = this.createLog(timestamp, owner, source, target, event);
-   return log;
- },
+  this.parseCombatLogEntry = function (log) {
+    var timeEntry = log.split(/::/),
+        timestamp = this.parseTimestamp(timeEntry[0]);
+        tokens = timeEntry[1].split(/,/);
+        owner = new nw.Actor(tokens[0], tokens[1]),
+        source = new nw.Actor(tokens[2], tokens[3]),
+        target = new nw.Actor(tokens[4], tokens[5]),
+        event = new nw.Event(tokens[6], tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]),
+        log = new nw.Entry(timestamp, owner, source, target, event);
+    return log;
+  },
 
- this.createActor = function (name, id) {
-   var actor = new Object();
-   actor.name = name;
-   actor.id = id;
-
-   return actor;
- },
-
- this.createEvent = function (name, id, type, flags, value, baseValue) {
-   var event = new Object();
-   event.name = name;
-   event.id = id;
-   event.type = type;
-   event.flags = flags.split('|').filter(function(flag) { return !(flag === "") });
-   event.value = value;
-   event.baseValue = baseValue;
-   return event;
- },
-
- this.createLog = function (timestamp, owner, source, target, event) {
-   var log = new Object();
-   log.timestamp = timestamp;
-   log.owner = owner;
-   if (source.id == "*") {
-     log.source = owner;
-   } else {
-     log.source = source;
-   }
-   if (target.id == "*") {
-     log.target = owner;
-   } else {
-     log.target = target;
-   }
-   log.event = event;
-   return log;
- },
-
- // convert the log timestamp format to ISO 8601
- this.parseTimestamp = function (logTime) {
-   var tokens = logTime.split(/:/),
-       year = tokens[0],
-       month = tokens[1],
-       day = tokens[2],
-       hour = tokens[3],
-       minute = tokens[4],
-       second = tokens[5],
-       timestamp = "20" + year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "00";
-   return new Date(timestamp);
- };
-
+  // convert the log timestamp format to ISO 8601
+  this.parseTimestamp = function (logTime) {
+    var tokens = logTime.split(/:/),
+        year = tokens[0],
+        month = tokens[1],
+        day = tokens[2],
+        hour = tokens[3],
+        minute = tokens[4],
+        second = tokens[5],
+        timestamp = "20" + year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "00";
+    return new Date(timestamp);
+  };
 }
 
+nw.Entry = function (timestamp, owner, source, target, event) {
+  this.timestamp = timestamp;
+  this.owner = owner;
+  this.source = source;
+  this.target = target;
+  this.event = event;
+
+  if (this.source.id == "*") {
+    this.source = owner;
+  }
+
+  if (this.target.id == "*") {
+    this.target = owner;
+  }
+}
+
+nw.Event = function (name, id, type, flags, value, baseValue) {
+  this.name = name;
+  this.id = id;
+  this.type = type;
+  this.flags = flags.split('|').filter(function(flag) { return !(flag === "") });
+  this.value = value;
+  this.baseValue = baseValue;
+
+  if (this.baseValue == 0) {
+    this.baseValue == value;
+  }
+}
+
+nw.Actor = function (name, id) {
+  this.name = name;
+  this.id = id;
+}
