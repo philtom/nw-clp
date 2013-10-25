701fcac19e3f25d1c419211d41230007d5a63f84
Phil Tom <phil@retrofitme.com>
2013-10-25 01:25:54 -0500

    damage board working now

--- src-web/damage-board.js
+++ src-web/damage-board.js
@@ -47,8 +47,54 @@ nwclp.DamageBoard = function(el, file) {
   },
 
   this.addEntry = function(entry) {
-    $(self.el).append(JSON.stringify(entry) + "<hr>")
-  };
+    //$(self.el).append(JSON.stringify(entry) + "<hr>")
+    // TODO cache log entries
+    //nw.logs.push(log);
+    if (entry.owner.id.indexOf('P') == 0) {
+      var row = self.getOrCreateRow(entry.owner);
+      var damageDone = $(row).children(".damage-done")[0];
+      $(damageDone).text(parseFloat($(damageDone).text()) + parseFloat(damage(entry)));
+    }
+  },
 
+  this.getOrCreateRow = function(actor) {
+    var divId = "#" + self.idToHtmlId(actor.id);
+    if ($(self.el).children(divId).length == 0) {
+      return self.createRow(actor.id, actor.name, 0, 0);
+    } else {
+      return $(self.el).children(divId)[0];
+    }
+  },
+
+  this.idToHtmlId = function(id) {
+    return id.replace(/[^a-zA-Z0-9_:.-]/g, '_');
+  },
+
+  this.createRow = function(id, name, damageDone, healsDone) {
+    var row = document.createElement("div");
+    $(row).attr("id", self.idToHtmlId(id))
+    $(row).addClass("row");
+    $(row).append(self.createCell(name, "name"));
+    $(row).append(self.createCell(parseInt(damageDone), "damage-done"));
+    $(row).append(self.createCell(parseInt(healsDone), "heals-done"));
+    $(self.el).append(row)
+    return row;
+  },
+
+  this.createCell = function(text, type) {
+    var cell = document.createElement("div");
+    $(cell).addClass("cell");
+    $(cell).addClass(type);
+    $(cell).text(text);
+    return cell;
+  },
+
+  this.damage = function(entry) {
+    if (entry.event.value > 0) {
+      return entry.event.value;
+    } else {
+      return 0;
+    }
+  };
 
 }
--- src-web/damage-by-player.js
+++ src-web/damage-by-player.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 });
 
 function asdf(evt) {
+  $("#players").empty();
+  // TOOD need to close the previous damage board file reader loop
   nwclp.board = new nwclp.DamageBoard($("#players"), evt.target.files[0]);
   nwclp.board.incrementalRead(nwclp.board);
 }
